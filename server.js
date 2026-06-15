c839cb71d3e195ca7fd477e19b49b9ae499300ef
Md. Imran Hossain <k.imran.khan464@gmail.com>
2026-06-15 12:21:18 +0600

    fix route

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const express = require("express");
+const app = express();
+
 function gcd(a, b) {
     while (b !== 0n) {
         [a, b] = [b, a % b];
@@ -10,18 +13,28 @@ function lcm(a, b) {
 }
 
 function isNatural(str) {
-    return /^[1-9]\d*$/.test(str);
+    return typeof str === "string" && /^[1-9]\d*$/.test(str);
 }
 
 app.get("/k_imran_khan464_gmail_com", (req, res) => {
     const { x, y } = req.query;
 
     if (!isNatural(x) || !isNatural(y)) {
-        return res.send("NaN");
+        return res.type("text/plain").send("NaN");
     }
 
     const a = BigInt(x);
     const b = BigInt(y);
 
-    res.send(lcm(a, b).toString());
+    res.type("text/plain").send(lcm(a, b).toString());
+});
+
+app.get("/", (req, res) => {
+    res.type("text/plain").send("OK");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
